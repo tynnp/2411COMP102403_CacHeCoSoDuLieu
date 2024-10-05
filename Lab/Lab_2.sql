00c625ee23afb35ae2c8f9a9a58d6f94d1b151bf
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-10-05 15:26:00 +0700

    một số câu phần proc

--- Lab/Lab_2.sql
+++ Lab/Lab_2.sql
@@ -1,4 +1,135 @@
+-- Cau 35
+CREATE PROCEDURE XINCHAO(@Ten NVARCHAR(MAX)) AS
+BEGIN 
+    PRINT N'Xin chào ' + @Ten
+END
 
+GO 
+XINCHAO N'Nguyễn Ngọc Phú Tỷ'
+
+-- Cau 36
+CREATE PROCEDURE TONG(@s1 int, @s2 int) AS
+BEGIN 
+    DECLARE @tg int 
+    SET @tg = @s1 + @s2
+    PRINT N'Tổng là: ' + CAST(@tg AS NVARCHAR)
+END 
+
+GO 
+TONG 2, 3
+
+-- Cau 37
+CREATE PROCEDURE TONG(@s1 int, @s2 int, @tong int output) AS
+BEGIN
+    SET @tong = @s1 + @s2
+    RETURN @tong 
+END 
+
+GO 
+DECLARE @tong int 
+SET @tong = 0
+EXECUTE TONG 2, 3, @tong out
+SELECT @tong AS TONG2SO
+
+-- Cau 38
+CREATE PROCEDURE SOLONNHAT(@s1 int, @s2 int) AS
+BEGIN 
+    DECLARE @max int 
+
+    IF @s1 > @s2 
+        SET @max = @s1
+    ELSE 
+        SET @max = @s2
+
+    PRINT N'Số lớn nhất của ' + CAST(@s1 AS NVARCHAR) + N' và ' + CAST(@s2 AS NVARCHAR) + N' là ' + CAST(@max AS NVARCHAR)
+END 
+
+GO 
+SOLONNHAT 2, 3
+
+-- Cau 39
+CREATE PROCEDURE MINVAMAX(@s1 int, @s2 int, @max int output, @min int output) AS 
+BEGIN 
+    IF @s1 > @s2 SELECT @max = @s1, @min = @s2
+    ELSE SELECT @max = @s2, @min = @s1
+    RETURN 
+END 
+
+GO 
+DECLARE @max int, @min int 
+SELECT @max = 0, @min = 0s
+EXECUTE MINVAMAX 2, 3, @max out, @min out 
+SELECT @max AS SOLON, @min AS SONHO
+
+-- Cau 40
+CREATE PROCEDURE TU_1_DEN_N (@n int) AS
+BEGIN 
+    DECLARE @i int, @out VARCHAR(MAX)
+    SELECT @i = 1, @out = ''
+
+    WHILE (@i <= @n) 
+        BEGIN 
+            SELECT @out = @out + CAST(@i AS VARCHAR) + ' ',
+            @i = @i + 1
+        END 
+
+    PRINT @out
+END
+
+GO 
+EXECUTE TU_1_DEN_N 5
+
+-- Cau 41
+CREATE PROCEDURE TONG_VA_SLSOCHAN(@n int) AS 
+BEGIN 
+    DECLARE @cnt int, @sum int, @i int 
+    SELECT @cnt = 0, @sum = 0, @i = 1
+
+    WHILE (@i <= @n) 
+        BEGIN 
+            SELECT @sum = @sum + @i
+            IF @i % 2 = 0 SET @cnt = @cnt + 1
+            SET @i = @i + 1
+        END 
+
+    PRINT N'Tổng: ' + CAST (@sum AS VARCHAR)
+    PRINT N'Số lượng số chẵn: ' + CAST(@cnt AS VARCHAR)
+END 
+
+GO 
+EXECUTE TONG_VA_SLSOCHAN 10
+
+-- Cau 42
+USE QLBongDa
+GO
+
+CREATE PROCEDURE SOTRANHOA AS
+BEGIN 
+    DECLARE @s int
+    SELECT @s = COUNT(*) FROM TRANDAU
+
+    WHERE VONG = 3 AND NAM = 2009 
+    AND LEFT(KETQUA, CHARINDEX('-', KETQUA) - 1) = RIGHT(KETQUA, LEN(KETQUA) - CHARINDEX('-', KETQUA))
+
+    PRINT N'Số trận hòa ở vòng 3 năm 2009 là ' + CAST(@s AS VARCHAR)
+END
+
+GO 
+EXECUTE SOTRANHOA
+
+-- Cau 43
+
+
+-- Cau 44
+
+
+-- Cau 45 
+
+
+-- Cau 46
+
+
+-- Cau 47
 
 
 -- Cau 48
