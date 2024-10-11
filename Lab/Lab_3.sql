e4c97b5a2d26ec73965d5f9c24b0c6941705e76c
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-10-11 16:30:43 +0700

    Câu 59 - Lab 3

--- Lab/Lab_3.sql
+++ Lab/Lab_3.sql
@@ -10,13 +10,37 @@ DECLARE @Mact NUMERIC(18, 0), @Ten NVARCHAR(100), @Vitri NVARCHAR(50)
 FETCH NEXT FROM POINT_CT INTO @Mact, @Ten, @Vitri
 
 WHILE @@FETCH_STATUS = 0
-    BEGIN 
-        PRINT N'Mã cầu thủ ' + CAST(@Mact AS VARCHAR(50))
-        PRINT N'Tên cầu thủ ' + @Ten 
-        PRINT N'Vị trí ' + @Vitri
-        PRINT ''
-        FETCH NEXT FROM POINT_CT INTO @Mact, @Ten, @Vitri
-    END
+BEGIN 
+    PRINT N'Mã cầu thủ ' + CAST(@Mact AS VARCHAR(50))
+    PRINT N'Tên cầu thủ ' + @Ten 
+    PRINT N'Vị trí ' + @Vitri
+    PRINT ''
+    FETCH NEXT FROM POINT_CT INTO @Mact, @Ten, @Vitri
+END
 
 CLOSE POINT_CT 
 DEALLOCATE POINT_CT
+
+-- Cau 59  ------------------------------------------------------------------------------------------------------------------------
+USE QLBongDa 
+GO 
+
+DECLARE POINT_CLB CURSOR FOR 
+SELECT CAULACBO.MACLB, CAULACBO.TENCLB, SANVD.TENSAN FROM CAULACBO
+JOIN SANVD ON CAULACBO.MASAN = SANVD.MASAN 
+
+OPEN POINT_CLB
+DECLARE @Maclb VARCHAR(5), @Tenclb NVARCHAR(100), @Tensan NVARCHAR(100)
+FETCH NEXT FROM POINT_CLB INTO @Maclb, @Tenclb, @Tensan 
+
+WHILE @@FETCH_STATUS = 0
+BEGIN 
+    PRINT N'Mã câu lạc bộ ' + @Maclb 
+    PRINT N'Tên câu lạc bộ ' + @Tenclb 
+    PRINT N'Tên sân ' + @Tensan
+    PRINT ''
+    FETCH NEXT FROM POINT_CLB INTO @Maclb, @Tenclb, @Tensan
+END
+
+CLOSE POINT_CLB
+DEALLOCATE POINT_CLB
